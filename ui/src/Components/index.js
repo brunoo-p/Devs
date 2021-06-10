70de5fd6c3c4c49d5716f9de680c024f6ee3b799
Bruno <71056568+brunoo-p@users.noreply.github.com>
2021-06-10 07:52:15 -0300

    Refactoring

--- ui/src/Components/index.js
+++ ui/src/Components/index.js
@@ -4,7 +4,5 @@ export { default as ScreenApp } from './ScreenApp';
 export { default as InteractiveButtons } from './InteractiveButtons';
 export { default as DetailsDev } from './DetailsDev';
 export { default as AccountConfiguration } from './AccountConfigurations';
-export { default as FormRegister } from './Forms/Register';
-export { default as FormLogin } from './Forms/Login';
 
 
